a6735e37d80c2c3fc461c10afbe97c6d76450c6e
root <root@DESKTOP-OQDM2N0.localdomain>
2019-05-20 13:50:18 -0400

    getting started with python

---
+++ test.py
@@ -0,0 +1,16 @@
+#Setup with data
+
+#For linear algebra purposes
+import numpy as np
+#For data processing, CSV file input/output
+import pandas as pd
+#Mathematical plotting tool
+import matplotlib.pyplot as plot
+#Visualization tool
+import seaborn
+
+import os
+
+#Input data files are available in "../input/" directory
+print(os.listdir("input"))
+
