c3cf8c6fa7b674ce0fee1004ceca3321a71ae6ed
Diana Soltani <31975705+DianaSoltani@users.noreply.github.com>
2019-05-26 13:59:02 -0400

    Added some introductory content with matplotlib.

--- test.py
+++ test.py
@@ -10,7 +10,25 @@ import matplotlib.pyplot as plot
 import seaborn
 
 import os
+#import json, csv, sys
 
-#Input data files are available in "../input/" directory
-print(os.listdir("input"))
+#Print all input data files that are available in the "input" directory
+print(os.listdir('input'))
 
+#Use pandas to read csv file
+data = pd.read_csv('input/Sarcasm_Headlines_Dataset.csv')
+
+#Dump all of the column information for the .csv file
+data.info()
+
+#
+data.corr()
+
+f,ax = plot.subplots(figsize = (18,18))
+seaborn.heatmap(data.corr(), annot=True, linewidths=.5, fmt= '.1f', ax=ax)
+plot.show()
+
+data.head(10)
+
+
+data.headline.plot()
